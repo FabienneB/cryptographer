4375066c3979ea71c0c2d842275eba231e7fab00
Fabienne <fabienne@fidzup.com>
2019-10-14 17:41:02 +0200

    change rot_algortithm algorithm to change the index of rotation

--- cryptographer.rb
+++ cryptographer.rb
@@ -1,20 +1,20 @@
-# Encrypt sentances using ROT-13 algorithm
+# Encrypt sentances using ROT-number algorithm
 # Using ASCII char
 # 65-90 => 'A' - 'Z'
 # 97-122 => 'a' - 'z'
 class Cryptographer
-  def rot_algortithm(string)
+  def rot_algortithm(string, number)
     string.each_char.each_with_object('') do |letter, encrypted_string|
       encrypted_string << letter if letter =~ /[^A-Za-z]/
       ascii_index = letter.ord
       case ascii_index
-      when 65..77, 97..109
-        encrypted_string << (ascii_index + 13).chr
-      when 78..90
-        new_ascii_index = 77 - (90 - ascii_index)
+      when 65..(90 - number), 97..(122 - number)
+        encrypted_string << (ascii_index + number).chr
+      when (91 - number)..90
+        new_ascii_index = (64 + number) - (90 - ascii_index)
         encrypted_string << new_ascii_index.chr
-      when 110..122
-        new_ascii_index = 109 - (122 - ascii_index)
+      when (123 - number)..122
+        new_ascii_index = (96 + number) - (122 - ascii_index)
         encrypted_string << new_ascii_index.chr
       end
     end
--- cryptographer_test.rb
+++ cryptographer_test.rb
@@ -9,33 +9,56 @@ class CryptographerTest < Minitest::Test
 
     describe 'encrypt messages' do
       it 'must encrypt the sentance' do
-        output = @cryptographer.rot_algortithm('MYA')
+        output = @cryptographer.rot_algortithm('MYA', 13)
         assert_equal 'ZLN', output
       end
 
       it 'must encrypt the sentance with 1 space' do
-        output = @cryptographer.rot_algortithm('My Message')
+        output = @cryptographer.rot_algortithm('My Message', 13)
         assert_equal 'Zl Zrffntr', output
       end
 
       it 'must encrypt the sentance with several spaces' do
-        output = @cryptographer.rot_algortithm('This is my secret')
+        output = @cryptographer.rot_algortithm('This is my secret', 13)
         assert_equal 'Guvf vf zl frperg', output
       end
 
       it 'must encrypt the sentance with number' do
-        output = @cryptographer.rot_algortithm('MY123')
+        output = @cryptographer.rot_algortithm('MY123', 13)
         assert_equal 'ZL123', output
       end
 
       it 'must be case sensitive' do
-        output = @cryptographer.rot_algortithm('MYmyMab')
+        output = @cryptographer.rot_algortithm('MYmyMab', 13)
         assert_equal 'ZLzlZno', output
       end
 
       it 'must decrypt the sentance with several spaces' do
-        output = @cryptographer.rot_algortithm('Guvf vf zl frperg')
+        output = @cryptographer.rot_algortithm('Guvf vf zl frperg', 13)
         assert_equal 'This is my secret', output
+      end
+    end
+  end
+
+  describe 'Encrypt/decrypt using ROT-6 algorithm' do
+    before do
+      @cryptographer = Cryptographer.new
+    end
+
+    describe 'encrypt messages' do
+      it 'must encrypt the sentance' do
+        output = @cryptographer.rot_algortithm('ABC', 6)
+        assert_equal 'GHI', output
+      end
+
+      it 'must encrypt the sentance with 1 space' do
+        output = @cryptographer.rot_algortithm('ABC xyz', 6)
+        assert_equal 'GHI def', output
+      end
+
+      it 'must encrypt the sentance with several spaces' do
+        output = @cryptographer.rot_algortithm('This is my secret', 6)
+        assert_equal 'Znoy oy se ykixkz', output
       end
     end
   end
